cc836c422ddcaf80ecdf5315357d6e0ccea5f805
proactivespirit <puddingbear0217@gmail.com>
2024-10-30 21:43:11 -0400

    feat: add po template page in detailed salesorder page

--- src/pages/dashboard/salesorder/detailed/billingInformation.js
+++ src/pages/dashboard/salesorder/detailed/billingInformation.js
@@ -1,354 +1,102 @@
-import { paramCase } from 'change-case';
-import { useState, useEffect } from 'react';
-// next
-import Head from 'next/head';
-import { useRouter } from 'next/router';
+import { useState } from 'react';
 // @mui
-import {
-  Card,
-  Table,
-  Button,
-  Tooltip,
-  TableBody,
-  Container,
-  IconButton,
-  TableContainer,
-} from '@mui/material';
-// redux
-import { useDispatch, useSelector } from 'src/redux/store';
-import { getProducts } from 'src/redux/slices/product';
-// routes
-import { PATH_DASHBOARD } from 'src/routes/paths';
-// components
-import { useSettingsContext } from 'src/components/settings';
-import {
-  useTable,
-  getComparator,
-  emptyRows,
-  TableNoData,
-  TableSkeleton,
-  TableEmptyRows,
-  TableHeadCustom,
-  TableSelectedAction,
-  TablePaginationCustom,
-} from 'src/components/table';
-import Iconify from 'src/components/iconify';
-import Scrollbar from 'src/components/scrollbar';
-import ConfirmDialog from 'src/components/confirm-dialog';
-// sections
-import { PurchaseOrderTableRow, PurchaseOrderTableToolbar } from 'src/sections/@dashboard/e-commerce/details/purchase';
+import { Box, Radio, TextField, Tooltip , RadioGroup, FormControlLabel, Container} from '@mui/material';
+import { Masonry } from '@mui/lab';
 
-// ----------------------------------------------------------------------
+import Label from 'src/components/label';
+import { DateTimePicker} from '@mui/x-date-pickers';
 
-const TABLE_HEAD = [
-  { id: 'number', label: 'Purchase Order Number', align: 'left', width: 300 },
-  { id: 'Product', label: 'Product', align: 'center' },
-  { id: 'Provider', label: 'Provider', align: 'center' },
-  { id: 'Region', label: 'Region', align: 'center' },
-  { id: 'CostIncVat', label: 'Cost Inc Vat', align: 'center' },
-  { id: 'Quantity', label: 'Quantity', align: 'center' },
-  { id: 'TotalCostIncVat', label: 'Total Cost Inc Vat', align: 'center' },
-  { id: 'created', label: 'Created On', align: 'center' },
+// import ExtendPrice from './extprice'
 
-];
-
-
-// ----------------------------------------------------------------------
-
-export default function BillingInformation() {
-  const {
-    dense,
-    page,
-    order,
-    orderBy,
-    rowsPerPage,
-    setPage,
-    //
-    selected,
-    setSelected,
-    onSelectRow,
-    onSelectAllRows,
-    //
-    onSort,
-    onChangeDense,
-    onChangePage,
-    onChangeRowsPerPage,
-  } = useTable({
-    defaultOrderBy: 'name',
+export default function BillingInformation({ variant }) {
+  const [values, setValues] = useState({
+    amount: '',
+    password: '',
+    weight: '',
+    weightRange: '',
+    showPassword: false,
   });
-
-  const { themeStretch } = useSettingsContext();
-
-  const { push } = useRouter();
-
-  const dispatch = useDispatch();
-
-  const { products, isLoading } = useSelector((state) => state.product);
-
-  const [tableData, setTableData] = useState([]);
-
-  const [filterName, setFilterName] = useState('');
-
-  const [filterStatus, setFilterStatus] = useState([]);
-
-  const [openConfirm, setOpenConfirm] = useState(false);
-
-  useEffect(() => {
-    dispatch(getProducts());
-  }, [dispatch]);
-
-  useEffect(() => {
-    if (products.length) {
-      setTableData(products);
-    }
-  }, [products]);
-
-  const dataFiltered = applyFilter({
-    inputData: tableData,
-    comparator: getComparator(order, orderBy),
-    filterName,
-    filterStatus,
-  });
-
-  const dataInPage = dataFiltered.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage);
-
-  const denseHeight = dense ? 60 : 80;
-
-  const isFiltered = filterName !== '' || !!filterStatus.length;
-
-  const isNotFound = (!dataFiltered.length && !!filterName) || (!isLoading && !dataFiltered.length);
-
-  const handleOpenConfirm = () => {
-    setOpenConfirm(true);
-  };
-
-  const handleCloseConfirm = () => {
-    setOpenConfirm(false);
-  };
-
-  const handleFilterName = (event) => {
-    setPage(0);
-    setFilterName(event.target.value);
-  };
-
-  const handleFilterStatus = (event) => {
-    const {
-      target: { value },
-    } = event;
-    setPage(0);
-    setFilterStatus(typeof value === 'string' ? value.split(',') : value);
-  };
-
-  const handleDeleteRow = (id) => {
-    const deleteRow = tableData.filter((row) => row.name !== id);
-    setSelected([]);
-    setTableData(deleteRow);
-
-    if (page > 0) {
-      if (dataInPage.length < 2) {
-        setPage(page - 1);
-      }
-    }
-  };
-
-  const handleDeleteRows = (selectedRows) => {
-    const deleteRows = tableData.filter((row) => !selectedRows.includes(row.name));
-    setSelected([]);
-    setTableData(deleteRows);
-
-    if (page > 0) {
-      if (selectedRows.length === dataInPage.length) {
-        setPage(page - 1);
-      } else if (selectedRows.length === dataFiltered.length) {
-        setPage(0);
-      } else if (selectedRows.length > dataInPage.length) {
-        const newPage = Math.ceil((tableData.length - selectedRows.length) / rowsPerPage) - 1;
-        setPage(newPage);
-      }
-    }
-  };
-
-  const handleEditRow = (id) => {
-    push(PATH_DASHBOARD.eCommerce.edit(paramCase(id)));
-  };
-
-  const handleViewRow = (id) => {
-    push(PATH_DASHBOARD.eCommerce.view(paramCase(id)));
-  };
-
-  const handleResetFilter = () => {
-    setFilterName('');
-    setFilterStatus([]);
-  };
 
   return (
     <>
-      <Head>
-        <title> Ecommerce: Product List | Minimal UI</title>
-      </Head>
-
-      <Container maxWidth={themeStretch ? false : 'mg'}>
-        {/* <CustomBreadcrumbs
-          heading="Product List"
-          links={[
-            { name: 'Dashboard', href: PATH_DASHBOARD.root },
-            {
-              name: 'E-Commerce',
-              href: PATH_DASHBOARD.eCommerce.root,
-            },
-            { name: 'List' },
-          ]}
-          action={
-            <Button
-              component={NextLink}
-              href={PATH_DASHBOARD.eCommerce.new}
-              variant="contained"
-              startIcon={<Iconify icon="eva:plus-fill" />}
-            >
-              New Product
-            </Button>
-          }
-        /> */}
-
-        <Card>
-          <PurchaseOrderTableToolbar
-            filterName={filterName}
-            filterStatus={filterStatus}
-            onFilterName={handleFilterName}
-            onFilterStatus={handleFilterStatus}
-            // statusOptions={STATUS_OPTIONS}
-            isFiltered={isFiltered}
-            onResetFilter={handleResetFilter}
+      <Container maxWidth={'md'} >
+        <Masonry columns={{ xs: 1}} spacing={2}>
+          <TextField
+            variant={variant}
+            required
+            fullWidth
+            label="Customer"
+            defaultValue="Dreamgame"
+          />
+          
+          <TextField
+            variant={variant}
+            required
+            fullWidth
+            type="email"
+            label="Email"
+            defaultValue="adam@dreamgame.com"
           />
 
-          <TableContainer sx={{ position: 'relative', overflow: 'unset' }}>
-            <TableSelectedAction
-              dense={dense}
-              numSelected={selected.length}
-              rowCount={tableData.length}
-              onSelectAllRows={(checked) =>
-                onSelectAllRows(
-                  checked,
-                  tableData.map((row) => row.name)
-                )
-              }
-              action={
-                <Tooltip title="Delete">
-                  <IconButton color="primary" onClick={handleOpenConfirm}>
-                    <Iconify icon="eva:trash-2-outline" />
-                  </IconButton>
-                </Tooltip>
-              }
-            />
-
-            <Scrollbar>
-              <Table size={dense ? 'small' : 'medium'} sx={{ minWidth: 1200 }}>
-                <TableHeadCustom
-                  order={order}
-                  orderBy={orderBy}
-                  headLabel={TABLE_HEAD}
-                  rowCount={tableData.length}
-                  numSelected={selected.length}
-                  onSort={onSort}
-                  onSelectAllRows={(checked) =>
-                    onSelectAllRows(
-                      checked,
-                      tableData.map((row) => row.name)
-                    )
-                  }
-                />
-
-                <TableBody>
-                  {(isLoading ? [...Array(rowsPerPage)] : dataFiltered)
-                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                    .map((row, index) =>
-                      row ? (
-                        <PurchaseOrderTableRow
-                          key={row.name}
-                          row={row}
-                          selected={selected.includes(row.name)}
-                          onSelectRow={() => onSelectRow(row.name)}
-                          onDeleteRow={() => handleDeleteRow(row.name)}
-                          onEditRow={() => handleEditRow(row.name)}
-                          onViewRow={() => handleViewRow(row.name)}
-                        />
-                      ) : (
-                        !isNotFound && <TableSkeleton key={index} sx={{ height: denseHeight }} />
-                      )
-                    )}
-
-                  <TableEmptyRows
-                    height={denseHeight}
-                    emptyRows={emptyRows(page, rowsPerPage, tableData.length)}
-                  />
-
-                  <TableNoData isNotFound={isNotFound} />
-                </TableBody>
-              </Table>
-            </Scrollbar>
-          </TableContainer>
-
-          <TablePaginationCustom
-            count={dataFiltered.length}
-            page={page}
-            rowsPerPage={rowsPerPage}
-            onPageChange={onChangePage}
-            onRowsPerPageChange={onChangeRowsPerPage}
-            //
-            dense={dense}
-            onChangeDense={onChangeDense}
+          <TextField
+            variant={variant}
+            required
+            fullWidth
+            label="Address"
+            defaultValue=""
           />
-        </Card>
+
+          <TextField
+            variant={variant}
+            required
+            fullWidth
+            label="ZipCode"
+            defaultValue=""
+          />
+
+          <TextField
+            variant={variant}
+            required
+            fullWidth
+            label="City"
+            defaultValue=""
+          />
+
+          <TextField
+            variant={variant}
+            required
+            fullWidth
+            label="State"
+            defaultValue=""
+          />
+
+          <TextField
+            variant={variant}
+            required
+            fullWidth
+            label="Country Code"
+            defaultValue="NL"
+          />
+
+          <TextField
+            variant={variant}
+            required
+            fullWidth
+            label="Region"
+            defaultValue="EU"
+          />
+
+          <TextField
+            variant={variant}
+            required
+            fullWidth
+            label="Default Currency"
+            defaultValue="EUR"
+          />
+        </Masonry>
+
       </Container>
-
-      <ConfirmDialog
-        open={openConfirm}
-        onClose={handleCloseConfirm}
-        title="Delete"
-        content={
-          <>
-            Are you sure want to delete <strong> {selected.length} </strong> items?
-          </>
-        }
-        action={
-          <Button
-            variant="contained"
-            color="error"
-            onClick={() => {
-              handleDeleteRows(selected);
-              handleCloseConfirm();
-            }}
-          >
-            Delete
-          </Button>
-        }
-      />
+      {/* <ExtendPrice /> */}
     </>
   );
-}
-
-// ----------------------------------------------------------------------
-
-function applyFilter({ inputData, comparator, filterName, filterStatus }) {
-  const stabilizedThis = inputData.map((el, index) => [el, index]);
-
-  stabilizedThis.sort((a, b) => {
-    const order = comparator(a[0], b[0]);
-    if (order !== 0) return order;
-    return a[1] - b[1];
-  });
-
-  inputData = stabilizedThis.map((el) => el[0]);
-
-  if (filterName) {
-    inputData = inputData.filter(
-      (product) => product.name.toLowerCase().indexOf(filterName.toLowerCase()) !== -1
-    );
-  }
-
-  if (filterStatus.length) {
-    inputData = inputData.filter((product) => filterStatus.includes(product.inventoryType));
-  }
-
-  return inputData;
 }
--- src/pages/dashboard/salesorder/detailed/purchaseOrder.js
+++ src/pages/dashboard/salesorder/detailed/purchaseOrder.js
@@ -1,354 +1,115 @@
-import { paramCase } from 'change-case';
-import { useState, useEffect } from 'react';
-// next
-import Head from 'next/head';
-import { useRouter } from 'next/router';
+import { useState } from 'react';
 // @mui
-import {
-  Card,
-  Table,
-  Button,
-  Tooltip,
-  TableBody,
-  Container,
-  IconButton,
-  TableContainer,
-} from '@mui/material';
-// redux
-import { useDispatch, useSelector } from 'src/redux/store';
-import { getProducts } from 'src/redux/slices/product';
-// routes
-import { PATH_DASHBOARD } from 'src/routes/paths';
-// components
-import { useSettingsContext } from 'src/components/settings';
-import {
-  useTable,
-  getComparator,
-  emptyRows,
-  TableNoData,
-  TableSkeleton,
-  TableEmptyRows,
-  TableHeadCustom,
-  TableSelectedAction,
-  TablePaginationCustom,
-} from 'src/components/table';
+import { Box, Autocomplete, TextField, Stack, InputAdornment, Container} from '@mui/material';
+import { Masonry } from '@mui/lab';
+
+import Label from 'src/components/label';
 import Iconify from 'src/components/iconify';
-import Scrollbar from 'src/components/scrollbar';
-import ConfirmDialog from 'src/components/confirm-dialog';
-// sections
-import { SalesOrderTableRow, SalesOrderTableToolbar } from 'src/sections/@dashboard/e-commerce/details/salesorder';
+import { DateTimePicker} from '@mui/x-date-pickers';
 
-// ----------------------------------------------------------------------
+// import ExtendPrice from './extprice'
 
-const TABLE_HEAD = [
-  { id: 'Number', label: 'Sales Order Number', align: 'left', width: 300 },
-  { id: 'Customer', label: 'Customer', align: 'center' },
-  // { id: 'price', label: 'PRICE', align: 'center' },
-  { id: 'Product', label: 'Product', align: 'center' },
-  { id: 'Provider', label: 'Provider', align: 'center' },
-  { id: 'Region', label: 'Region', align: 'center' },
-  { id: 'vat', label: 'Sales Inc Vat', align: 'center' },
-  { id: 'status', label: 'Quantity', align: 'center' },
-  { id: 'created', label: 'Created On', align: 'center' },
-];
-
-
-// ----------------------------------------------------------------------
-
-export default function PurchaseOrder() {
-  const {
-    dense,
-    page,
-    order,
-    orderBy,
-    rowsPerPage,
-    setPage,
-    //
-    selected,
-    setSelected,
-    onSelectRow,
-    onSelectAllRows,
-    //
-    onSort,
-    onChangeDense,
-    onChangePage,
-    onChangeRowsPerPage,
-  } = useTable({
-    defaultOrderBy: 'name',
+export default function BillingInformation({ variant }) {
+  const [values, setValues] = useState({
+    amount: '',
+    password: '',
+    weight: '',
+    weightRange: '',
+    showPassword: false,
   });
-
-  const { themeStretch } = useSettingsContext();
-
-  const { push } = useRouter();
-
-  const dispatch = useDispatch();
-
-  const { products, isLoading } = useSelector((state) => state.product);
-
-  const [tableData, setTableData] = useState([]);
-
-  const [filterName, setFilterName] = useState('');
-
-  const [filterStatus, setFilterStatus] = useState([]);
-
-  const [openConfirm, setOpenConfirm] = useState(false);
-
-  useEffect(() => {
-    dispatch(getProducts());
-  }, [dispatch]);
-
-  useEffect(() => {
-    if (products.length) {
-      setTableData(products);
-    }
-  }, [products]);
-
-  const dataFiltered = applyFilter({
-    inputData: tableData,
-    comparator: getComparator(order, orderBy),
-    filterName,
-    filterStatus,
-  });
-
-  const dataInPage = dataFiltered.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage);
-
-  const denseHeight = dense ? 60 : 80;
-
-  const isFiltered = filterName !== '' || !!filterStatus.length;
-
-  const isNotFound = (!dataFiltered.length && !!filterName) || (!isLoading && !dataFiltered.length);
-
-  const handleOpenConfirm = () => {
-    setOpenConfirm(true);
-  };
-
-  const handleCloseConfirm = () => {
-    setOpenConfirm(false);
-  };
-
-  const handleFilterName = (event) => {
-    setPage(0);
-    setFilterName(event.target.value);
-  };
-
-  const handleFilterStatus = (event) => {
-    const {
-      target: { value },
-    } = event;
-    setPage(0);
-    setFilterStatus(typeof value === 'string' ? value.split(',') : value);
-  };
-
-  const handleDeleteRow = (id) => {
-    const deleteRow = tableData.filter((row) => row.name !== id);
-    setSelected([]);
-    setTableData(deleteRow);
-
-    if (page > 0) {
-      if (dataInPage.length < 2) {
-        setPage(page - 1);
-      }
-    }
-  };
-
-  const handleDeleteRows = (selectedRows) => {
-    const deleteRows = tableData.filter((row) => !selectedRows.includes(row.name));
-    setSelected([]);
-    setTableData(deleteRows);
-
-    if (page > 0) {
-      if (selectedRows.length === dataInPage.length) {
-        setPage(page - 1);
-      } else if (selectedRows.length === dataFiltered.length) { 
-        setPage(0);
-      } else if (selectedRows.length > dataInPage.length) {
-        const newPage = Math.ceil((tableData.length - selectedRows.length) / rowsPerPage) - 1;
-        setPage(newPage);
-      }
-    }
-  };
-
-  const handleEditRow = (id) => {
-    push(PATH_DASHBOARD.eCommerce.edit(paramCase(id)));
-  };
-
-  const handleViewRow = (id) => {
-    push(PATH_DASHBOARD.eCommerce.view(paramCase(id)));
-  };
-
-  const handleResetFilter = () => {
-    setFilterName('');
-    setFilterStatus([]);
-  };
 
   return (
     <>
-      <Head>
-        <title> Ecommerce: Product List | Minimal UI</title>
-      </Head>
-
-      <Container maxWidth={themeStretch ? false : 'mg'}>
-        {/* <CustomBreadcrumbs
-          heading="Product List"
-          links={[
-            { name: 'Dashboard', href: PATH_DASHBOARD.root },
-            {
-              name: 'E-Commerce',
-              href: PATH_DASHBOARD.eCommerce.root,
-            },
-            { name: 'List' },
-          ]}
-          action={
-            <Button
-              component={NextLink}
-              href={PATH_DASHBOARD.eCommerce.new}
-              variant="contained"
-              startIcon={<Iconify icon="eva:plus-fill" />}
-            >
-              New Product
-            </Button>
-          }
-        /> */}
-
-        <Card>
-          <SalesOrderTableToolbar
-            filterName={filterName}
-            filterStatus={filterStatus}
-            onFilterName={handleFilterName}
-            onFilterStatus={handleFilterStatus}
-            // statusOptions={STATUS_OPTIONS}
-            isFiltered={isFiltered}
-            onResetFilter={handleResetFilter}
+      <Container maxWidth={'md'} >
+        <Masonry columns={{ xs: 1}} spacing={2}>
+          <TextField
+            variant={variant}
+            required
+            fullWidth
+            label="Quantity"
+            defaultValue="100"
           />
-
-          <TableContainer sx={{ position: 'relative', overflow: 'unset' }}>
-            <TableSelectedAction
-              dense={dense}
-              numSelected={selected.length}
-              rowCount={tableData.length}
-              onSelectAllRows={(checked) =>
-                onSelectAllRows(
-                  checked,
-                  tableData.map((row) => row.name)
-                )
-              }
-              action={
-                <Tooltip title="Delete">
-                  <IconButton color="primary" onClick={handleOpenConfirm}>
-                    <Iconify icon="eva:trash-2-outline" />
-                  </IconButton>
-                </Tooltip>
-              }
+          
+          <Stack spacing={2} direction={{ xs: 'column', sm: 'row' }}>
+            <TextField
+              variant={"outlined"}
+              fullWidth
+              value={"38"}
+              // onChange={handleChange('weight')}
+              label="Sales"
+              // helperText="Weight"
+              InputProps={{
+                endAdornment: <InputAdornment position="start">EUR</InputAdornment>,
+              }}
             />
+            <TextField
+              variant={"outlined"}
+              fullWidth
+              value={"36"}
+              // onChange={handleChange('weight')}
+              label="Expected Cost"
+              // helperText="Weight"
+              InputProps={{
+                endAdornment: <InputAdornment position="end">EUR</InputAdornment>,
+              }}
+            />
+          </Stack>
+          <Stack spacing={2} direction={{ xs: 'column', sm: 'row' }}>
+            <DateTimePicker
+              renderInput={(props) => <TextField {...props} fullWidth />}
+              label="Start Date"
+              // value={value}
+              // onChange={setValue}
+            />
+            <DateTimePicker
+              renderInput={(props) => <TextField {...props} fullWidth />}
+              label="End Date"
+              // value={value}
+              // onChange={setValue}
+            />
+          </Stack>
 
-            <Scrollbar>
-              <Table size={dense ? 'small' : 'medium'} sx={{ minWidth: 1200 }}>
-                <TableHeadCustom
-                  order={order}
-                  orderBy={orderBy}
-                  headLabel={TABLE_HEAD}
-                  rowCount={tableData.length}
-                  numSelected={selected.length}
-                  onSort={onSort}
-                  onSelectAllRows={(checked) =>
-                    onSelectAllRows(
-                      checked,
-                      tableData.map((row) => row.name)
-                    )
-                  }
-                />
-
-                <TableBody>
-                  {(isLoading ? [...Array(rowsPerPage)] : dataFiltered)
-                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                    .map((row, index) =>
-                      row ? (
-                        <SalesOrderTableRow
-                          key={row.name}
-                          row={row}
-                          selected={selected.includes(row.name)}
-                          onSelectRow={() => onSelectRow(row.name)}
-                          onDeleteRow={() => handleDeleteRow(row.name)}
-                          onEditRow={() => handleEditRow(row.name)}
-                          onViewRow={() => handleViewRow(row.name)}
-                        />
-                      ) : (
-                        !isNotFound && <TableSkeleton key={index} sx={{ height: denseHeight }} />
-                      )
-                    )}
-
-                  <TableEmptyRows
-                    height={denseHeight}
-                    emptyRows={emptyRows(page, rowsPerPage, tableData.length)}
-                  />
-
-                  <TableNoData isNotFound={isNotFound} />
-                </TableBody>
-              </Table>
-            </Scrollbar>
-          </TableContainer>
-
-          <TablePaginationCustom
-            count={dataFiltered.length}
-            page={page}
-            rowsPerPage={rowsPerPage}
-            onPageChange={onChangePage}
-            onRowsPerPageChange={onChangeRowsPerPage}
-            //
-            dense={dense}
-            onChangeDense={onChangeDense}
+          <Autocomplete
+            multiple
+            fullWidth
+            options={top100Films}
+            getOptionLabel={(option) => option.title}
+            defaultValue={[top100Films[1]]}
+            filterSelectedOptions
+            renderInput={(params) => (
+              <TextField {...params} label="Template Regions" placeholder="Country Code" />
+            )}
           />
-        </Card>
-      </Container>
+          <Stack spacing={2} direction={{ xs: 'column', sm: 'row' }}>
+            <Label variant="filled" color="warning" startIcon={<Iconify icon="eva:email-fill" />}>
+              Save & Generate Auto-Calculated
+            </Label>
+            <Label variant="filled" color="primary" startIcon={<Iconify icon="eva:email-fill" />}>
+              Save & Generate Empty
+            </Label>
+            <Label variant="filled" color="error" startIcon={<Iconify icon="eva:email-fill" />}>
+              Generate POs from the template
+            </Label>
+          </Stack>
+        </Masonry>
 
-      <ConfirmDialog
-        open={openConfirm}
-        onClose={handleCloseConfirm}
-        title="Delete"
-        content={
-          <>
-            Are you sure want to delete <strong> {selected.length} </strong> items?
-          </>
-        }
-        action={
-          <Button
-            variant="contained"
-            color="error"
-            onClick={() => {
-              handleDeleteRows(selected);
-              handleCloseConfirm();
-            }}
-          >
-            Delete
-          </Button>
-        }
-      />
+      </Container>
+      {/* <ExtendPrice /> */}
     </>
   );
 }
 
+
 // ----------------------------------------------------------------------
 
-function applyFilter({ inputData, comparator, filterName, filterStatus }) {
-  const stabilizedThis = inputData.map((el, index) => [el, index]);
-
-  stabilizedThis.sort((a, b) => {
-    const order = comparator(a[0], b[0]);
-    if (order !== 0) return order;
-    return a[1] - b[1];
-  });
-
-  inputData = stabilizedThis.map((el) => el[0]);
-
-  if (filterName) {
-    inputData = inputData.filter(
-      (product) => product.name.toLowerCase().indexOf(filterName.toLowerCase()) !== -1
-    );
-  }
-
-  if (filterStatus.length) {
-    inputData = inputData.filter((product) => filterStatus.includes(product.inventoryType));
-  }
-
-  return inputData;
-}
+export const top100Films = [
+  { title: 'BE', year: 1994 },
+  { title: 'DE', year: 1972 },
+  { title: 'ES', year: 1974 },
+  { title: 'FR', year: 2008 },
+  { title: 'NL', year: 1957 },
+  { title: "PT", year: 1993 },
+  { title: 'PL', year: 1994 },
+  { title: 'NO', year: 2003 },
+  { title: 'GB', year: 1966 },
+];
