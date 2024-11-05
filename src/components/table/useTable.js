1d439f67fed4568ff5c6436993e31317034b9a53
proactivespirit <puddingbear0217@gmail.com>
2024-11-05 11:57:41 -0500

    fix: fix product table page rowperrproduct

--- src/components/table/useTable.js
+++ src/components/table/useTable.js
@@ -58,7 +58,6 @@ export default function useTable(props) {
   }, []);
 
   const onChangePage = useCallback((event, newPage) => {
-    console.log("rowsPerPage" , rowsPerPage);
     setPage(newPage);
   }, []);
 
