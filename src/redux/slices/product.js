f5b8c0f7b6ece6e41ea880a96b4dd5bb32d65e36
proactivespirit <puddingbear0217@gmail.com>
2024-11-14 11:31:29 -0500

    fix: dashboard error display

--- src/redux/slices/product.js
+++ src/redux/slices/product.js
@@ -201,7 +201,7 @@ export function getProducts() {
       console.log('Nintendo : ', Nexway);
       // console.log('Epay : ', Epay.data.products);
 
-      const response = [...Nintendo.data.products, ...Epay.data.products, ...Nexway.data.products];
+      const response = [...Nexway.data.products, ...Nintendo.data.products, ...Epay.data.products ];
       dispatch(slice.actions.getProductsSuccess(response));
     } catch (error) {
       console.log("error: " , error);
