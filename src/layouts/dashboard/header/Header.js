533969050e5fcbb0d9bf6b0b3bc539a8002c996f
proactivespirit <puddingbear0217@gmail.com>
2024-12-03 13:03:07 -0500

    fix: remove navbar country

--- src/layouts/dashboard/header/Header.js
+++ src/layouts/dashboard/header/Header.js
@@ -57,7 +57,7 @@ export default function Header({ onOpenNav }) {
         justifyContent="flex-end"
         spacing={{ xs: 0.5, sm: 1.5 }}
       >
-        <LanguagePopover />
+        {/* <LanguagePopover /> */}
 
         <NotificationsPopover />
 
